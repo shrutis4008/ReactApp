8fb500c6b7c3959e5f2d95b149b352274a274934
shruti <ssuresh22.haw.iit.edu>
2024-04-30 10:02:27 +0530

    commit with router dom configurations

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,11 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div> 
+       <Navbar/>
+       <Home/>
+       <Footer/>
+       </div>
+    },
+    {
+      path: "about",
+      element: <div>About</div>,
+    },
+  ]);
+
   return (
-    <div>
-      Hello World.
-    </div>
+    // <div>
+    //   Hello World.
+    // </div>
+ 
+  <RouterProvider router={router} />
   );
 }
 
