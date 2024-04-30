ca63e753d75831e8f1d078419aa86740a106d42d
shruti <ssuresh22.haw.iit.edu>
2024-04-30 20:14:48 +0530

    commit

--- frontend/my-app/src/components/NavigationBar.jsx
+++ frontend/my-app/src/components/NavigationBar.jsx
@@ -11,8 +11,8 @@ const NavigationBar = () => {
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Login</Nav.Link>
+            <Nav.Link href="#pricing">Write</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
--- frontend/my-app/src/pages/Home.jsx
+++ frontend/my-app/src/pages/Home.jsx
@@ -1,11 +1,62 @@
 import React from 'react'
+import { Link, useLocation } from "react-router-dom";
 
 const Home = () => {
+
+  const posts =[
+    {
+      id: 1, 
+      title: "What is Lorem Ipsum?",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+      img: "frontend/my-app/image/pexels-ash-122861-376464.jpg"
+      
+    },
+
+    {
+      id: 2, 
+      title: "What is Lorem Ipsum?",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+      img: "frontend/my-app/image/pexels-fotios-photos-1279330.jpg"
+      
+    },
+
+    {
+      id: 1, 
+      title: "What is Lorem Ipsum?",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+      img: "frontend/my-app/image/pexels-ash-122861-376464.jpg"
+      
+    },
+
+    {
+      id: 1, 
+      title: "What is Lorem Ipsum?",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+      img: "frontend/my-app/image/pexels-ash-122861-376464.jpg"
+      
+    }
+  ]
+
   return (
-    <div>
-      This is the homepage.
+    <div className="home">
+    <div className="posts">
+      {posts.map((post) => (
+        <div className="post" key={post.id}>
+          <div className="img">
+            <img src={`../upload/${post.img}`} alt="" />
+          </div>
+          <div className="content">
+            <Link className="link" to={`/post/${post.id}`}>
+              <h1>{post.title}</h1>
+            </Link>
+            <p>{getText(post.desc)}</p>
+            <button>Read More</button>
+          </div>
+        </div>
+      ))}
     </div>
-  )
-}
+  </div>
+);
+};
 
 export default Home
