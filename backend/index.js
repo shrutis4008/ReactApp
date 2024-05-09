bb006db389b5b0cf967d6a7214f516419e37ec43
shruti <ssuresh22.hawk.iit.edu>
2024-05-09 11:23:36 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const corsOptions = {
   origin: "http://localhost:3000/",
   credentials: true,
   optionSuccessStatus: 200,
-  accessControlAllowOrigin: "*",
+  accessControlAllowOrigin: "http://localhost:3000/",
   accessControlAllowCredentials: true,
   accessControlAllowHeaders:
     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
