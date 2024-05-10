0fd56d78ddba6ceca20298574f36be343291e480
shruti <ssuresh22.hawk.iit.edu>
2024-05-10 15:48:47 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -4,21 +4,51 @@ import connectDatabase from "./config/database.js";
 import dotenv from "dotenv";
 import express from "express";
 
-app.use(express());
 app.use(cors());
 
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://react-1flkd5rrp-shrutis-projects-226bf37e.vercel.app"
+    "https://react-blog-app-ixe0.onrender.com/",
+    "*"
   );
   res.header("Access-Control-Allow-Methods", "OPTIONS, POST,GET, PUT, DELETE");
-  res.header("Cross-Origin", "True");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  next();
 });
+
+// const allowedOrigins = ["https://react-blog-app-ixe0.onrender.com/"];
+
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     methods: "GET,POST,PUT,DELETE,HEAD,PATCH",
+//     allowedHeaders:
+//       "Access-Control-Allow-Headers,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers",
+//     credentials: true,
+//   })
+// );
+
+//
+
+// app.use(function (req, res, next) {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://react-1flkd5rrp-shrutis-projects-226bf37e.vercel.app"
+//   );
+//   res.header("Access-Control-Allow-Methods", "OPTIONS, POST,GET, PUT, DELETE");
+//   res.header("Cross-Origin", "True");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+// });
 
 // app.use(cors());
 
@@ -26,13 +56,11 @@ app.use(function (req, res, next) {
 
 // const corsOptions = {
 //   origin: "http://localhost:3000/",
-//   credentials: true,
 //   optionSuccessStatus: 200,
 //   accessControlAllowOrigin: "*",
 //   accessControlAllowCredentials: true,
-//   accessControlAllowHeaders:
-//     "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
-//   methods: "OPTIONS, POST,GET, PUT",
+//   accessControlAllowHeaders: "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+//   methods: "OPTIONS, POST, GET, PUT, DELETE",
 // };
 
 //
